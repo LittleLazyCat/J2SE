d558953b6d5da966ad92d17aebb34e4b654835d4
Little-Lazy-Cat <850162412@qq.com>
2016-12-07 23:04:12 +0800

    工厂模式

---
+++ Factory/src/factory/NYPizzaStore.java
@@ -0,0 +1,12 @@
+package factory;
+
+public class NYPizzaStore extends PizzaStore {
+
+	@Override
+	protected Pizza createPizza(String type) {
+		if("cheese".equals(type)){
+			return new NYStyleCheesePizza();
+		}else return null;
+	}
+
+}
